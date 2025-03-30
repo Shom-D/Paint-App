15ca40e50430adfb90d388ced3a4ab882c4ce216
Shom-D <soham.dubey@outlook.com>
2025-03-30 11:43:11 +0100

    Add files via upload

--- paint.py
+++ paint.py
@@ -19,35 +19,49 @@ class PaintApp:
         self.canvas = tk.Canvas(self.root)
         self.canvas.pack()
 
-        self.width_slider = ttk.Scrollbar(self.frame, command= self.change_width)
+        self.width_slider = ttk.Scale(self.frame, from_= 1, to = 20)
+        self.width = self.width_slider.get()
+    
         self.width_slider.grid(row = 0, column= 3)
-        self.width = 1
+        
 
         self.b_c = ttk.Button(self.frame, text = "Pick a colour", command = self.change_colour)
         self.b_c.grid(row = 0, column= 4)
-        self.colour = "#ffffff"
+        self.colour = "#000000"
+        
+        self.background_colour = "#ffffff"
+        self.canvas.config(bg = self.background_colour)
+
+        self.b_r = ttk.Button(self.frame, text = "Reset Canvas", command= self.clear_canvas)
+        self.b_r.grid(row = 0, column= 5)
 
         self.canvas.bind("<ButtonPress-1>", self.activate)
         self.canvas.bind("<B1-Motion>", self.draw)
         self.canvas.bind("<ButtonRelease-1>", self.reset)
+
+        self.x = self.y = None
+
         self.root.mainloop()
 
     def change_colour(self):
         self.colour = askcolor(color = self.colour)[1]
         print(self.colour)
 
-    def change_width(self):
-        pass    
+    def clear_canvas(self):
+        self.canvas.delete("all")
+        pass
 
     def activate(self, event):
         self.active = True
 
     def draw(self, event):
-        if not self.x and self.y:
+        if not (self.x and self.y):
             self.x, self.y = event.x, event.y
         
         colour = self.colour if self.type else self.background_colour
-        self.canvas.create_line(self.x, self.y, event.x, event.y, width= self.width)
+        self.width = self.width_slider.get()
+
+        self.canvas.create_line(self.x, self.y, event.x, event.y, width= self.width, fill = colour)
         
         
         self.x, self.y = event.x, event.y
